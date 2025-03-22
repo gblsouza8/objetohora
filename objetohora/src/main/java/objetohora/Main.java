e5dd7050fee5164951feb28c82a4d977c95485da
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-21 22:03:56 -0300

    Quarto Commit

--- objetohora/src/main/java/objetohora/Main.java
+++ objetohora/src/main/java/objetohora/Main.java
@@ -3,8 +3,22 @@ package objetohora;
 public class Main {
     public static void main(String[] args) {
         Hora hora = new Hora();
-        hora.setHora(21, 58, 59);
+        try {
+            hora.setHora(21, 58, 59);
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
+        }
+        System.out.println(hora.toString());
 
-        System.out.printf(hora.toString());
+        Hora hora2 = new Hora();
+
+        try {
+            hora2.setHora(25, 58, 59);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("%s%n", e.getMessage());
+        }
+
+        System.out.println(hora2.toString());
+
     }
 }
