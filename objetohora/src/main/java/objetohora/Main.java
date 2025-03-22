dfe902e157bf82498b7d50df437d52540c4f61b6
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-21 21:59:35 -0300

    Terceiro Commit

--- objetohora/src/main/java/objetohora/Main.java
+++ objetohora/src/main/java/objetohora/Main.java
@@ -2,6 +2,9 @@ package objetohora;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hora hora = new Hora();
+        hora.setHora(21, 58, 59);
+
+        System.out.printf(hora.toString());
     }
 }
